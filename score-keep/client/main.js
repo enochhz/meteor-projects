612c1f6a071bfa7840edaf88b586fa3a32f6b803
walkerzheng <9771330@qq.com>
2017-07-14 15:25:55 -0700

    update

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,4 +1,17 @@
-import someDefault, {name} from './../imports/utils';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('Log from ./../client/main.js');
-console.log(someDefault);
+Meteor.startup(function(){
+    let name = 'Walker';
+    // title -> Account Settings
+    let title = 'Socre Keep';
+    let jsx = (
+        <div>
+            <h1>{title}</h1>
+            <p>Hello {name}</p>
+            <p>Second p.</p>
+        </div>
+    );
+    ReactDom.render(jsx, document.getElementById('app'));
+});
