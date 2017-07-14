6e26f3cf73be9d1b2c8ec1da29cdacbbed73c7dd
walkerzheng <9771330@qq.com>
2017-07-14 13:39:41 -0700

    create score-keeper project

---
+++ score-keep/client/main.js
@@ -0,0 +1,7 @@
+import {greetUser, name} from './../imports/utils';
+import {add} from './../imports/math.js';
+
+console.log('Log from /client/main.js');
+console.log(greetUser());
+console.log(name);
+console.log(add(2, 4));
---
+++ score-keep/imports/utils.js
@@ -0,0 +1,8 @@
+console.log('Log from /imports/utils.js');
+
+export let greetUser = function() {
+    return 'Hello';
+};
+
+export let name = 'Walker Zheng';
+
