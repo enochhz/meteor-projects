7420a0e703366b0e1b665c85ca4413d4137ff9db
walkerzheng <9771330@qq.com>
2017-07-25 13:14:45 -0700

    5/44 Styling Player with Flewbox

--- score-keep/imports/ui/AddPlayer.js
+++ score-keep/imports/ui/AddPlayer.js
@@ -19,8 +19,8 @@ export default class AddPlayer extends React.Component {
     render() {
         return (
             <div className="item">
-                <form onSubmit={this.handleSubmit.bind(this)}>
-                    <input type='text' name='playerName' placeholder='Player name'/>
+                <form className="form" onSubmit={this.handleSubmit.bind(this)}>
+                    <input className='form_input' name='playerName' placeholder='Player name'/>
                     <button className="button">Add Player</button>
                 </form>
             </div>
