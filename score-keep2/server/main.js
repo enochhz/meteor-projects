68419c270134c90edef4fdf384481adb23720a1c
walkerzheng <walkerhaozheng@gmail.com>
2017-08-15 19:36:39 -0700

    3-20 ES6 Aside-Arrow Functions

--- score-keep2/server/main.js
+++ score-keep2/server/main.js
@@ -7,4 +7,23 @@ Meteor.startup(function(){
     //     score: 45
     // });
     // console.log(Players.find().fetch());
+    // let square = (x, y) => x * y;
+    // let square2 = function(x, y){
+    //     return x + y;
+    // }
+    // console.log(square(3,5));
+    // console.log(square2(3,5));
+    //
+    // let user = {
+    //     name: 'Andrew',
+    //     sayHi () {
+    //         setTimeout(() => {
+    //            console.log(this.name);
+    //         }, 1000);
+    //     }
+    // };
+    // user.sayHi(1, 2);
+    let numbers = [9, 99, 4, 56];
+    let newNumbers = numbers.map((number)=> number + 1);
+    console.log(newNumbers);
 });
