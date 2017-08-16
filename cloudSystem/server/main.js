9affdce9c81da9a49f0d01ab91a8db53cafc1158
walkerzheng <walkerhaozheng@gmail.com>
2017-08-16 11:56:35 -0700

    create cloud system project

---
+++ cloudSystem/server/main.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import {Patients} from './../imports/api/patients';
+
+Meteor.startup(() => {
+   // Patients.insert({
+   //     name: 'Walker',
+   //     age: 15,
+   //      visitTimes: 0
+   // });
+   // console.log(Patients.find().fetch());
+});
