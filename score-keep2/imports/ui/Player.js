baa74107f33b14073b25b9382bec201752579e21
walkerzheng <walkerhaozheng@gmail.com>
2017-08-25 10:58:33 -0700

    5-48 Styling the color of first three componets

--- score-keep2/imports/ui/Player.js
+++ score-keep2/imports/ui/Player.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import {Players} from './../api/player';
+
 import PropTypes from 'prop-types';
 
 export default class Player extends React.Component{
     render() {
-        return <div className="item" key={this.props.player._id}>
+        let itemClassName = `item item--position-${this.props.player.rank}`;
+
+        return <div className={itemClassName} key={this.props.player._id}>
             <div className="player">
                 <div>
                     <h3 className="player_name">{this.props.player.name}</h3>
                     <p className="player__status">
-                        {this.props.player.rank} {this.props.player.position} {this.props.player.score} point(s).</p>
+                        {this.props.player.rank} place - {this.props.player.position} {this.props.player.score} point(s).</p>
                 </div>
                 <div className="player__action">
                     <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id}, {$inc: {score: -1}})}>-1</button>
