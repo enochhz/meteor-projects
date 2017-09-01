981e1ef08808585f4b109dc52ad65d68ca09b2cc
walkerzheng <walkerhaozheng@gmail.com>
2017-09-01 12:27:54 -0700

    6-54 Setting Up Routes

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -1,11 +1,30 @@
 import {Meteor} from 'meteor/meteor';
-import React from 'react';
+import React    from 'react';
 import ReactDom from 'react-dom';
 
-import Signup from './../imports/ui/Signup';
-import Link from './../imports/ui/Link';
+import {Router, Route, Switch} from 'react-router';
 
+import Login    from './../imports/ui/Login';
+import Signup   from './../imports/ui/Signup';
+import Link     from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+
+const browserHistory = require('history').createBrowserHistory();
+
+// Create and Import basic version of Login
+// Create the route for "/" and render Login
+
+const routes = (
+    <Router history={browserHistory}>
+        <Switch>
+            <Route path="/" component={Login}/>
+            <Route path="/signup" component={Signup}/>
+            <Route path="/links" component={Link}/>
+            <Route path="*" component={NotFound}/>
+        </Switch>
+    </Router>
+);
 
 Meteor.startup(() => {
-    ReactDom.render(<Link/>, document.getElementById('app'));
+    ReactDom.render(routes, document.getElementById('app'));
 });
