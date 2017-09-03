c45bda1e107a97d421b5a987358296bac5c36b82
walkerzheng <walkerhaozheng@gmail.com>
2017-09-03 08:00:38 -0700

    6-56 Component State and Signup Form

--- short-lnk/imports/ui/Signup.js
+++ short-lnk/imports/ui/Signup.js
@@ -2,10 +2,28 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        this.setState({
+            error: 'Something went wrong.'
+        });
+    }
     render() {
        return (
            <div>
               <h1>Join Us</h1>
+               {this.state.error ? <p>{this.state.error}</p> : undefined}
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" name="email" placeholder="Email"/>
+                    <input type="password" name="password" placeholder="Password"/>
+                    <button>Sumbmit</button>
+                </form>
               <Link to='/'>Already Have an account? </Link>
            </div>
        );
