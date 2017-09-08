8772d6cc1253359aa162f9b156b36b2dc3703194
walkerzheng <9771330@qq.com>
2017-09-08 10:40:41 -0700

    6-63 New User Validation

--- short-lnk/imports/ui/Signup.js
+++ short-lnk/imports/ui/Signup.js
@@ -14,6 +14,11 @@ export default class Signup extends React.Component {
         e.preventDefault();
         let email = this.refs.email.value.trim();
         let password = this.refs.password.value.trim();
+
+        if (password.length < 9) {
+            return this.setState({error: 'Password must be more than 8 characters long'});
+        }
+
         Accounts.createUser({email, password}, (err) => {
             console.log('Singup callback', err);
             if (err) {
@@ -28,7 +33,7 @@ export default class Signup extends React.Component {
            <div>
               <h1>Sign Up New Account</h1>
                {this.state.error ? <p>{this.state.error}</p> : undefined}
-                <form onSubmit={this.onSubmit.bind(this)}>
+                <form onSubmit={this.onSubmit.bind(this)} noValidate>
                     <input type="email" ref="email" name="email" placeholder="Email"/>
                     <input type="password" ref="password" name="password" placeholder="Password"/>
                     <button>Sumbmit</button>
