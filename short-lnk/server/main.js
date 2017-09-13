18a141522ee477f0ea38bc982fd756c524b40cd0
walkerzheng <walkerhaozheng@gmail.com>
2017-09-13 16:45:36 -0700

    7-77 short id

--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,21 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import {WebApp}   from 'meteor/webapp';
+import {Links}    from '../imports/api/links';
 import '../imports/api/links';
 import '../imports/api/users';
 import './../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
-    WebApp.connectHandlers.use((req, res, next) => {
-        console.log('This is from my custom middleware');
-        console.log(req.url, req.method, req.headers, req.query);
-        // Set HTTP status code
-        // res.statusCode = 404;
-        // Set HTTP headers
-        // res.setHeader('my-custom-header', 'Andrew was there');
-        // Set HTTP body
-        // res.write('<h1>This is my middleware</h1>');
-        // End HTTP request
-        // res.end();
-        next();
-    });
+    // WebApp.connectHandlers.use((req, res, next) => {
+    //     const _id = req.url.slice(1);
+    //     console.log(_id);
+    //     const link = Links.find({_id});
+    //     if (link) {
+    //         res.statusCode = 302;
+    //         res.setHeader('Location', link.url);
+    //         res.end();
+    //     } else {
+    //        next()
+    //     }
+    // });
 });
