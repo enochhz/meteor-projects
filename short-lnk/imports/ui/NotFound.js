8cd32aecd71dff8aa61248db9fa6f2930d461fd9
walkerzheng <walkerhaozheng@gmail.com>
2017-09-19 17:02:13 -0700

    8-89 Stateless Functional Components

--- short-lnk/imports/ui/NotFound.js
+++ short-lnk/imports/ui/NotFound.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-    render() {
-        return <p>not found</p>;
-    }
+export default () => {
+    return <p>not found</p>;
 }
